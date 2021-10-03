a8fdd3ec5eeacc04d910376e76d04f26f7f59aab
Mateusz <matesz1kb@gmail.com>
2021-10-03 11:10:05 +0200

    api key placed in .env

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,12 +3,10 @@ require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
-console.log(apiKey);
-
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID WH9lEbseq_9EaeFSxRa7mesbwg5I-pK1lP-BUustitY",
+    Authorization: apiKey,
   }
 });
 
